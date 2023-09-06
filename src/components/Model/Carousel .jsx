3b2d4a0fc865d4622dde1ca7da401495e11159d0
khaled nashwan <khaled.nashwan.1997@gmail.com>
2023-09-06 11:54:11 +0300

    upload

--- src/components/Model/Carousel .jsx
+++ src/components/Model/Carousel .jsx
@@ -10,8 +10,7 @@ import { Autoplay, Navigation } from "swiper";
 import Pro from "./Pro";
 import phote1 from "../../assets/coffe.jpg";
 import phote2 from "../../assets/career.jpg";
-import phote3 from "../../assets/Capture3.PNG";
-import phote4 from "../../assets/Capture4.PNG";
+import phote3 from "../../assets/WhatsApp Image 2023-09-06 at 11.37.54.jpg";
 import phote5 from "../../assets/Screenshot.jpg";
 import phote6 from "../../assets/fitness.jpg";
 
@@ -73,12 +72,7 @@ const Carousel  = () => {
             desc="landing page with responsive design and tailwindcss "
           />
         </SwiperSlide>
-        <SwiperSlide>
-          <Pro img={phote4}
-          
-          desc="Sign in with html css js"
-          />
-        </SwiperSlide>
+
         <SwiperSlide>
           <Pro
             img={phote5}
